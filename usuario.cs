4b3b5946aeaff13fce5b32ac869c8ce569a75d9d
IsabellaAlvesOli <135767037+IsabellaAlvesOli@users.noreply.github.com>
2024-04-24 21:30:06 -0300

    Add files via upload

---
+++ usuario.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace Tcc
+{
+    #region Usuario
+    public class Usuario
+    {
+        #region Member Variables
+        protected int _codigo;
+        protected string _nome;
+        protected string _email;
+        protected string _telefone;
+        protected string _senha;
+        #endregion
+        #region Constructors
+        public Usuario() { }
+        public Usuario(string nome, string email, string telefone, string senha)
+        {
+            this._nome=nome;
+            this._email=email;
+            this._telefone=telefone;
+            this._senha=senha;
+        }
+        #endregion
+        #region Public Properties
+        public virtual int Codigo
+        {
+            get {return _codigo;}
+            set {_codigo=value;}
+        }
+        public virtual string Nome
+        {
+            get {return _nome;}
+            set {_nome=value;}
+        }
+        public virtual string Email
+        {
+            get {return _email;}
+            set {_email=value;}
+        }
+        public virtual string Telefone
+        {
+            get {return _telefone;}
+            set {_telefone=value;}
+        }
+        public virtual string Senha
+        {
+            get {return _senha;}
+            set {_senha=value;}
+        }
+        #endregion
+    }
+    #endregion
+}
