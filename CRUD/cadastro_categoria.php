b7dd51f08308a4f84e3e00fc032c25ad4e176256
Daniel Teixeira <89745887+DanielZTZ@users.noreply.github.com>
2024-06-01 00:16:19 -0300

    Consertando problemas no Include

--- CRUD/cadastro_categoria.php
+++ CRUD/cadastro_categoria.php
@@ -1,40 +1,39 @@
 <?php
 session_start();
-include_once("conexao.php");
+include_once("../conexao.php");
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+      <meta charset="UTF-8">
+  </head>
+  <body> <p><hr> <p>
+      <center> <h1> Cadastro de Categorias de Produtos </h1> <br> 
+    <?php
+        if(isset($_SESSION['msg']))
+        {
+          echo $_SESSION['msg'];
+          unset($_SESSION['msg']);
+        }
+    ?>    
+      <h2>
+      <form method="POST" action="recebe2.php">
+      <!--  <label>Código:
+        <input type="text" name="codigo" placeholder="Digite o código"><br><br>
+        -->   
+        <label>Nome: </label>
+      <input type="text" name="nome" placeholder="Insira o nome da categoria de produtos"><br><br>
 
- <!DOCTYPE html>
- <html lang="pt-br">
-     <head>
-        <meta charset="UTF-8">
-   </head>
-   <body> <p><hr> <p>
-        <center> <h1> Cadastro de Categorias de Produtos </h1> <br> 
-      <?php
-          if(isset($_SESSION['msg']))
-          {
-            echo $_SESSION['msg'];
-            unset($_SESSION['msg']);
-          }
-      ?>    
-        <h2>
-        <form method="POST" action="recebe2.php">
-        <!--  <label>Código:
-          <input type="text" name="codigo" placeholder="Digite o código"><br><br>
-         -->   
-         <label>Nome: </label>
-        <input type="text" name="nome" placeholder="Insira o nome da categoria de produtos"><br><br>
+      <input type="submit"  value="Cadastrar"> <br> <br> <br> <br> <br> <br>
+    </form> 
+      
+    <hr> <p> 
+    <a href="Principal.php"> <input type="button"  value="Página Inicial"></a> &nbsp
+  <!--  <a href="cadastro.php"> <input type="button"  value= "Cadastrar"></a> &nbsp --> 
+    <a href= "exe_listar2.php"> <input type="button"  value="Listar" ></a> &nbsp
+    <!-- <a href= "exe_listar.php"> <input type="button"  value="Listar Dados Completo"></a> &nbsp
+    <a href= "exe_listar2.php"> <input type="button"  value="Alterar"></a> &nbsp
+    <a href="apaga.php"><input type="button"  value="Apagar"></a> &nbsp <br> --> 
 
-        <input type="submit"  value="Cadastrar"> <br> <br> <br> <br> <br> <br>
-      </form> 
-       
-     <hr> <p> 
-      <a href="Principal.php"> <input type="button"  value="Página Inicial"></a> &nbsp
-    <!--  <a href="cadastro.php"> <input type="button"  value= "Cadastrar"></a> &nbsp --> 
-      <a href= "exe_listar2.php"> <input type="button"  value="Listar" ></a> &nbsp
-     <!-- <a href= "exe_listar.php"> <input type="button"  value="Listar Dados Completo"></a> &nbsp
-      <a href= "exe_listar2.php"> <input type="button"  value="Alterar"></a> &nbsp
-      <a href="apaga.php"><input type="button"  value="Apagar"></a> &nbsp <br> --> 
-
-    </body>
-    </html>
+  </body>
+  </html>
