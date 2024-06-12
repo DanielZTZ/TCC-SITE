165cf24feeefdf990481dd965ed152669365908a
spi12310 <165228327+spi12310@users.noreply.github.com>
2024-06-12 16:10:13 -0300

    Create vendas_listar.php

---
+++ vendas_listar.php
@@ -0,0 +1,83 @@
+<?php
+session_start();
+include_once("conexao.php");
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>CRUD - Listar</title>		
+	</head>
+	<body>
+		<hr> <p>
+		<a href="principal_crud.php"> <input type="button"  value="Página Inicial"></a> &nbsp
+    	
+        
+		<p><hr><h1>Lista de Vendas</h1> <p> <hr> <h3>
+		<?php
+		if(isset($_SESSION['msg']))
+		{
+			echo $_SESSION['msg'];
+			unset($_SESSION['msg']);
+			echo "<br>";
+		}
+		
+		//Receber o número da página
+		$pagina_atual = filter_input(INPUT_GET,'pagina', FILTER_SANITIZE_NUMBER_INT);		
+		$pagina = (!empty($pagina_atual)) ? $pagina_atual : 1;
+		
+		//Setar a quantidade de itens por pagina
+		$qnt_result_pg = 3;
+		
+		//calcular o inicio visualização
+		$inicio = ($qnt_result_pg * $pagina) - $qnt_result_pg;
+		
+		$result_usuarios = "SELECT * FROM vendas LIMIT $inicio, $qnt_result_pg";
+		$resultado_usuarios = mysqli_query($conn, $result_usuarios);
+		while($row_usuario = mysqli_fetch_assoc($resultado_usuarios)){
+			echo "  ID da venda: " . $row_usuario['id_venda'] . "<br>";
+			echo "  Nome do usuário: " . $row_usuario['id_usuario'] . "<br>";
+			echo "  ID Produto: " . $row_usuario['id_produto'] . "<br>";
+			echo "  Quantidade: " . $row_usuario['quantidade'] . "<br>";
+			echo "  Preço: " . $row_usuario['preco_unitario'] . "<br>";
+			echo "  Data da venda: " . $row_usuario['data_venda'] . "<br>";
+
+
+
+			echo "<hr> <br>";
+
+		//	echo "<a href='editar_aluno.php?id=" . $row_usuario['id'] . "'>Editar</a><br>";
+		//	echo "<a href='proc_apagar_usuario.php?id=" . $row_usuario['id'] . "'>Apagar</a><br><hr>";
+		}
+		
+		//Paginção - Somar a quantidade de usuários
+		$result_pg = "SELECT COUNT(id_venda) AS num_result FROM vendas";
+		$resultado_pg = mysqli_query($conn, $result_pg);
+		$row_pg = mysqli_fetch_assoc($resultado_pg);
+		//echo $row_pg['num_result'];
+		//Quantidade de pagina 
+		$quantidade_pg = ceil($row_pg['num_result'] / $qnt_result_pg);
+		
+		//Limitar os link antes depois
+		$max_links = 2;
+		echo "<a href='vendas_listar.php?pagina=1'>Primeira</a> ";
+		
+		for($pag_ant = $pagina - $max_links; $pag_ant <= $pagina - 1; $pag_ant++){
+			if($pag_ant >= 1){
+				echo "<a href='vendas_listar.php?pagina=$pag_ant'>$pag_ant</a> ";
+			}
+		}
+			
+		echo "$pagina ";
+		
+		for($pag_dep = $pagina + 1; $pag_dep <= $pagina + $max_links; $pag_dep++){
+			if($pag_dep <= $quantidade_pg){
+				echo "<a href='vendas_listar.php?pagina=$pag_dep'>$pag_dep</a> ";
+			}
+		}
+		
+		echo "<a href='vendas_listar.php?pagina=$quantidade_pg'>Ultima</a>";
+		
+		?>		
+	</body>
+</html>
